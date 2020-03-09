acbf209563afa0e0f93dd048d74cd7c2713fcb32
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-03-09 15:53:23 -0400

    connected images to html

--- script.js
+++ script.js
@@ -6,34 +6,62 @@
 
 
 
-const apiTester = async () => {
+// const imageDiv = document.getElementById('img-div');
 
+const button = document.querySelector('#btn');
+const inputAddress = document.querySelector('input');
+const title = document.querySelector(".display1");
+const overview = document.querySelector(".display2");
+const posterUrl = document.querySelector(".display3");
+const movieId = document.querySelector(".display4");
+const image = document.querySelector(".hero-logo")
 
-  // const response = await axios.get("https://api.themoviedb.org/3/movie/550?api_key=8d021868bbab84ae4f9d16fdc0645e0c");
+button.addEventListener('click', async () => {
+  let address = inputAddress.value;
+  // debugger
+  let response = await axios.get(`${address}`);
+  console.log(response.data);
 
-  const response = await axios.get("https://api.themoviedb.org/3/search/movie?api_key=8d021868bbab84ae4f9d16fdc0645e0c&query=star+wars");
-
-  // console.log(response.data);
-
-  const title = document.querySelector(".display1");
-  const overview = document.querySelector(".display2");
-  const posterUrl = document.querySelector(".display3");
-  const movieId = document.querySelector(".display4");
   title.innerHTML += response.data.results[5].title;
   overview.innerHTML += response.data.results[5].overview;
   posterUrl.innerHTML += response.data.results[5].poster_path;
   movieId.innerHTML += response.data.results[5].id;
+  image.setAttribute("src", `https://image.tmdb.org/t/p/w500/${response.data.results[5].poster_path}`);
 
 
-  // https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=8d021868bbab84ae4f9d16fdc0645e0c&language=en-US
+});
 
-  // https://image.tmdb.org/t/p/w500/mMZRKb3NVo5ZeSPEIaNW9buLWQ0.jpg
 
-  const response2 = await axios.get("https://swapi.co/api/starships/");
 
-  console.log(response2.data);
 
-};
+// const apiTester = async () => {
 
-apiTester();
+
+//   // const response = await axios.get("https://api.themoviedb.org/3/movie/550?api_key=8d021868bbab84ae4f9d16fdc0645e0c");
+
+//   const response = await axios.get("https://api.themoviedb.org/3/search/movie?api_key=8d021868bbab84ae4f9d16fdc0645e0c&query=star+wars");
+
+//   console.log(response.data);
+
+//   const title = document.querySelector(".display1");
+//   const overview = document.querySelector(".display2");
+//   const posterUrl = document.querySelector(".display3");
+//   const movieId = document.querySelector(".display4");
+//   title.innerHTML += response.data.results[5].title;
+//   overview.innerHTML += response.data.results[5].overview;
+//   posterUrl.innerHTML += response.data.results[5].poster_path;
+//   movieId.innerHTML += response.data.results[5].id;
+
+
+//   // https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=8d021868bbab84ae4f9d16fdc0645e0c&language=en-US
+
+//   // https://image.tmdb.org/t/p/w500/mMZRKb3NVo5ZeSPEIaNW9buLWQ0.jpg
+
+//   const response2 = await axios.get("https://swapi.co/api/starships/");
+
+//   console.log(response2.data);
+
+// };
+
+// apiTester();
 
